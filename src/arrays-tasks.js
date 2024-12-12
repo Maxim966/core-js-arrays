1bc385def0ad2c80aa736b3c598e21bcc0e7135b
Maksim Sukhov <silveraven@yandex.ru>
2024-12-12 15:38:43 +0200

    feat: add solution createNDimensionalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -313,8 +313,13 @@ function distinct(arr) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  const array = Array(size).fill(0);
+  if (n === 1) {
+    return array;
+  }
+
+  return array.map(() => createNDimensionalArray(n - 1, size));
 }
 
 /**
